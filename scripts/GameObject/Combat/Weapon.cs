75268c99a70e7c0cd477d6928fdaefc1330254ef
firebelley <firebelley@users.noreply.github.com>
2019-12-09 20:03:56 -0500

    add hot indicator

--- scripts/GameObject/Combat/Weapon.cs
+++ scripts/GameObject/Combat/Weapon.cs
@@ -5,12 +5,17 @@ namespace Deathville.GameObject.Combat
 {
     public class Weapon : Node2D
     {
+        private const float OVERHEAT_DECAY_DELAY = 2f;
         private const string ANIM_FIRE = "fire";
 
         [Signal]
         public delegate void Fired();
         [Signal]
         public delegate void HeatChanged(Weapon weapon);
+        [Signal]
+        public delegate void Overheated(Weapon weapon);
+        [Signal]
+        public delegate void Cooled(Weapon weapon);
 
         [Export]
         private NodePath _projectileSpawnerComponentPath;
@@ -51,8 +56,9 @@ namespace Deathville.GameObject.Combat
         private float _currentHeat;
         private float _decayTracker = 0f;
         private bool _canDecay = true;
+        private bool _overheated = false;
 
-        private float _fireTime = 0f;
+        private float _timeToNextShot = 0f;
 
         private AnimationPlayer _animationPlayer;
         private Sprite _sprite;
@@ -73,28 +79,36 @@ namespace Deathville.GameObject.Combat
 
         public override void _Process(float delta)
         {
-            _fireTime = Mathf.Clamp(_fireTime - delta / (IsPlayer ? Engine.TimeScale : 1f), 0f, float.MaxValue);
-            UpdateCooldown();
+            _timeToNextShot = Mathf.Clamp(_timeToNextShot - delta / (IsPlayer ? Engine.TimeScale : 1f), 0f, float.MaxValue);
             if (IsPlayer)
             {
                 _animationPlayer.PlaybackSpeed = 1f / Engine.TimeScale;
+            }
+
+            if (_overheated)
+            {
+                UpdateCooldownOverheated();
+            }
+            else
+            {
+                UpdateCooldownStandard();
             }
         }
 
         public void AttemptFire(Vector2 atTarget)
         {
-            if (_fireTime == 0f && CurrentHeat < 1f)
+            if (!_overheated && _timeToNextShot == 0f && CurrentHeat < 1f)
             {
                 _projectileSpawnerComponent?.Spawn(IsPlayer, atTarget);
             }
         }
 
-        private void UpdateCooldown()
+        private void UpdateCooldownStandard()
         {
             if (IsPlayer)
             {
                 var delta = GetProcessDeltaTime();
-                _decayTracker = Mathf.Clamp(_decayTracker - delta / Engine.TimeScale, 0f, _heatDecayDelay);
+                _decayTracker = Mathf.Clamp(_decayTracker - delta / Engine.TimeScale, 0f, float.MaxValue);
                 _canDecay = _decayTracker == 0f;
                 if (CurrentHeat > 0f && _canDecay)
                 {
@@ -102,6 +116,25 @@ namespace Deathville.GameObject.Combat
                     var divisor = CurrentHeat * _heatDecayMultiplier;
                     var totalDelta = Mathf.Clamp(deltaDecay / divisor, deltaDecay, float.MaxValue);
                     CurrentHeat = Mathf.Clamp(CurrentHeat - totalDelta, 0f, 1f);
+                }
+            }
+        }
+
+        private void UpdateCooldownOverheated()
+        {
+            if (IsPlayer)
+            {
+                var delta = GetProcessDeltaTime();
+                _decayTracker = Mathf.Clamp(_decayTracker - delta / Engine.TimeScale, 0f, float.MaxValue);
+                _canDecay = _decayTracker == 0f;
+                if (CurrentHeat > 0f && _canDecay)
+                {
+                    CurrentHeat = Mathf.Clamp(CurrentHeat - _heatDecayPerSecond * delta / Engine.TimeScale, 0f, 1f);
+                    if (CurrentHeat == 0f)
+                    {
+                        _overheated = false;
+                        EmitSignal(nameof(Cooled), this);
+                    }
                 }
             }
         }
@@ -117,13 +150,22 @@ namespace Deathville.GameObject.Combat
 
         private void OnProjectileSpawned()
         {
-            _fireTime = 1f / _projectilesPerSecond;
+            _timeToNextShot = 1f / _projectilesPerSecond;
             if (IsPlayer)
             {
                 CurrentHeat = Mathf.Clamp(CurrentHeat + _heatPerShot, 0f, 1f);
                 _decayTracker = _heatDecayDelay;
+                if (CurrentHeat == 1f)
+                {
+                    _overheated = true;
+                    _decayTracker = OVERHEAT_DECAY_DELAY;
+                }
             }
             EmitSignal(nameof(Fired));
+            if (_overheated)
+            {
+                EmitSignal(nameof(Overheated), this);
+            }
         }
     }
 }
--- scripts/UI/WeaponUI.cs
+++ scripts/UI/WeaponUI.cs
@@ -1,5 +1,6 @@
 using Deathville.GameObject.Combat;
 using Godot;
+using GodotApiTools.Extension;
 
 namespace Deathville.UI
 {
@@ -8,6 +9,7 @@ namespace Deathville.UI
 
         private const string ANIM_GROW = "grow";
         private const string ANIM_SHRINK = "shrink";
+        private const string ANIM_DEFAULT = "default";
 
         public Weapon CurrentWeapon
         {
@@ -28,45 +30,75 @@ namespace Deathville.UI
         private Weapon _currentWeapon;
         private TextureRect _textureRect;
         private Label _hotLabel;
-        private AnimationPlayer _animationPlayer;
+        private AnimationPlayer _swapAnimationPlayer;
+        private AnimationPlayer _hotAnimationPlayer;
 
         public override void _Ready()
         {
             _overheatBar = GetNode<OverheatBar>("OverheatBar");
             _textureRect = GetNode<TextureRect>("TextureRect");
             _hotLabel = GetNode<Label>("TextureRect/HotLabel");
-            _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+            _swapAnimationPlayer = GetNode<AnimationPlayer>("SwapAnimationPlayer");
+            _hotAnimationPlayer = GetNode<AnimationPlayer>("HotAnimationPlayer");
 
+            _hotLabel.Hide();
             UpdatePivotOffset();
         }
 
         public override void _Process(float delta)
         {
-            _animationPlayer.PlaybackSpeed = 1 / Engine.TimeScale;
+            _swapAnimationPlayer.PlaybackSpeed = 1f / Engine.TimeScale;
+            _hotAnimationPlayer.PlaybackSpeed = 1f / Engine.TimeScale;
         }
 
         public void ConnectWeapon(Weapon weapon)
         {
+            DisconnectWeapon();
             _currentWeapon = null;
             _textureRect.Texture = null;
             _currentWeapon = weapon;
             _textureRect.Texture = _currentWeapon.Sprite.Texture;
             _overheatBar.ConnectWeapon(_currentWeapon);
+
+            weapon.Connect(nameof(Weapon.Overheated), this, nameof(OnWeaponOverheated));
+            weapon.Connect(nameof(Weapon.Cooled), this, nameof(OnWeaponCooled));
         }
 
         public void PlaySwapAnimation()
         {
-            _animationPlayer.Play(ANIM_GROW, -1f, 1f);
+            _swapAnimationPlayer.Play(ANIM_GROW, -1f, 1f);
         }
 
         public void PlayStowAnimation()
         {
-            _animationPlayer.Play(ANIM_SHRINK, -1f, 1f);
+            _swapAnimationPlayer.Play(ANIM_SHRINK, -1f, 1f);
+        }
+
+        private void DisconnectWeapon()
+        {
+            if (IsInstanceValid(_currentWeapon))
+            {
+                this.DisconnectAllSignals(_currentWeapon);
+            }
         }
 
         private void UpdatePivotOffset()
         {
             RectPivotOffset = new Vector2(_isLeft ? RectSize.x : 0f, RectSize.y);
+        }
+
+        private void OnWeaponOverheated(Weapon weapon)
+        {
+            _hotAnimationPlayer.Play(ANIM_DEFAULT);
+        }
+
+        private void OnWeaponCooled(Weapon weapon)
+        {
+            if (_hotAnimationPlayer.IsPlaying())
+            {
+                _hotAnimationPlayer.Seek(1f, true);
+            }
+            _hotAnimationPlayer.Stop();
         }
     }
 }
